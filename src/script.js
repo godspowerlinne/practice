d5e24268974e5d5f80b8329100b68a54a95c32c8
Godspower <amriccygodspea@gmail.com>
2024-11-26 21:56:15 +0100

    feat: fix

--- src/script.js
+++ src/script.js
@@ -9,3 +9,21 @@ menuIcon.addEventListener("click", () => {
     showMenu.classList.toggle("hidden");
     closeMenu.classList.toggle("hidden");
 });
+
+// Header menu
+const headerIcon = document.getElementById("headerIcon");
+const headerList = document.getElementById('headerList');
+const closeHeaderIcon = document.getElementById('closeHeaderIcon');
+
+
+headerIcon.onclick = () =>{
+    // console.log('Header menu clicked');
+    headerList.classList.toggle("flex_show");
+    headerIcon.style.display = "none";
+};
+
+closeHeaderIcon.onclick = () => {
+    // console.log('Close header menu clicked');
+    headerList.classList.remove("flex_show");
+    headerIcon.style.display = "block";
+}
